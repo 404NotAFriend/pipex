1ed0426b357bc9d4a30967d5a54bb52853cb980e
404NotAFriend <brunodrcgomes@gmail.com>
2026-01-29 15:32:20 +0000

    Created project structure and folders , created first base files , started header file with the includes and structs

---
+++ includes/pipex.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 06:37:07 by bramalho@st       #+#    #+#             */
+/*   Updated: 2026/01/29 15:28:43 by bramalho@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+/* ========================================================================== */
+/*                              INCLUDES                                      */
+/* ========================================================================== */
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+
+/* ========================================================================== */
+/*                              ERROR MESSAGES                                */
+/* ========================================================================== */
+
+# define ERR_ARGS "Invalid number of arguments"
+# define ERR_INFILE "Infile"
+# define ERR_OUTFILE "Outfile"
+# define ERR_PIPE "Pipe"
+# define ERR_CMD "Command not found"
+
+/* ========================================================================== */
+/*                              MAIN  STRUCTURE                               */
+/* ========================================================================== */
+
+typedef struct s_pipex
+{
+	char	**envp;
+	char	**av;
+	char	**cmd_args;
+	char	*cmd_path;
+	int		ac;
+	int		here_doc;
+	int		infile;
+	int		outfile;
+	int		pipe_fd[2];
+	int		i;
+
+} t_pipex;
+
+/* ========================================================================== */
+/*                              CLEANUP & EXIT                                */
+/* ========================================================================== */
+
+
+
+#endif
