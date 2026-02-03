1005aeb62e070b92ecb83bc8c0433aae78e9dce8
404NotAFriend <brunodrcgomes@gmail.com>
2026-02-03 00:37:03 +0000

    created here_doc function for the bonus of Typing instead of Infile transfer with limiter , created pipex_bonus to handle mutiple files , added new funcs and gnl declaration to header , fixed some issues

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,8 +6,46 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:01:44 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/01/29 18:36:04 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/02/03 00:13:21 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+static void	read_input(char *limiter, int *fd)
+{
+	char	*line;
+
+	close(fd[0]);
+	while (1)
+	{
+		line = get_next_line(0);
+		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
+		{
+			free(line);
+			exit(0);
+		}
+		write(fd[1], line, ft_strlen(line));
+		free(line);
+	}
+}
+
+void	here_doc(char *limiter, int ac)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (ac < 6)
+		msg_error("Invalid number of arguments");
+	if (pipe(fd) == -1)
+		msg_error("Pipe failed");
+	pid = fork();
+	if (pid == 0)
+		read_input(limiter, fd);
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		wait(NULL);
+	}
+}
--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,8 +6,79 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:05:13 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/01/29 18:36:11 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/02/03 00:19:41 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+static void	child_process(t_pipex *data)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (pipe(fd) == -1)
+		msg_error("Pipe failed");
+	pid = fork();
+	if (pid == -1)
+		msg_error("Fork failed");
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		exec(data);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+static void	handle_input(t_pipex *data, int ac, char **av)
+{
+	if (ft_strncmp(av[1], "here_doc", 8) == 0)
+	{
+		data->i = 3;
+		data->outfile = open_file(av[ac - 1], 2);
+		here_doc(av[2], ac);
+	}
+	else
+	{
+		data->i = 2;
+		data->outfile = open_file(av[ac - 1], 1);
+		data->infile = open_file(av[1], 0);
+		dup2(data->infile, STDIN_FILENO);
+	}
+}
+
+static void	init_bonus(t_pipex *data, int ac, char **av, char **envp)
+{
+	data->ac = ac;
+	data->av = av;
+	data->envp = envp;
+	data->cmd_args = NULL;
+	data->cmd_path = NULL;
+	data->cmd_paths = NULL;
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_pipex	data;
+
+	if (ac < 5)
+		return (msg_error("Invalid number of arguments"), 1);
+	init_bonus(&data, ac, av, envp);
+	handle_input(&data, ac, av);
+	parse_cmds(&data);
+	while (data.i < ac - 2)
+	{
+		child_process(&data);
+		data.i++;
+	}
+	dup2(data.outfile, STDOUT_FILENO);
+	exec(&data);
+	parent_free(&data);
+	return (0);
+}
