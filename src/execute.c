5ad284e0e670e157663f7cd56f9f68843fc664e5
404NotAFriend <brunodrcgomes@gmail.com>
2026-02-03 20:22:20 +0000

    fixed all tests and leaks , problems fixed in execute.c and pipex_bonus. normi fixed

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:00:33 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/02/03 02:37:34 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/02/03 19:55:50 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,12 @@ static char	*get_cmd(char **paths, char *cmd)
 	return (NULL);
 }
 
-static void	free_cmd_args(char **cmd_args)
-{
-	int	i;
-
-	i = 0;
-	if (!cmd_args)
-		return ;
-	while (cmd_args[i])
-	{
-		free(cmd_args[i]);
-		i++;
-	}
-	free(cmd_args);
-}
-
 void	exec(t_pipex *data)
 {
 	if (!data->av[data->i] || data->av[data->i][0] == '\0')
 	{
 		ft_putendl_fd("pipex: command not found: ", 2);
+		parent_free(data);
 		exit(127);
 	}
 	data->cmd_args = ft_split(data->av[data->i], ' ');
@@ -66,14 +52,12 @@ void	exec(t_pipex *data)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
 		ft_putendl_fd(data->cmd_args[0], 2);
-		free_cmd_args(data->cmd_args);
-		child_free(data);
+		parent_free(data);
 		exit(127);
 	}
 	if (execve(data->cmd_path, data->cmd_args, data->envp) == -1)
 	{
-		free_cmd_args(data->cmd_args);
-		free(data->cmd_path);
+		parent_free(data);
 		msg_error("pipex: execve failed");
 	}
 }
--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,17 +6,34 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:05:13 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/02/03 02:36:50 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/02/03 20:18:29 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+static void	child_handler(t_pipex *data, int *fd)
+{
+	close(fd[0]);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
+	if (data->i == 2)
+	{
+		if (data->infile == -1)
+		{
+			parent_free(data);
+			exit(1);
+		}
+		dup2(data->infile, STDIN_FILENO);
+		close(data->infile);
+	}
+	exec(data);
+}
+
 static void	child_process(t_pipex *data)
 {
 	pid_t	pid;
 	int		fd[2];
-	int		input_fd;
 
 	if (pipe(fd) == -1)
 		msg_error("Pipe failed");
@@ -24,18 +41,7 @@ static void	child_process(t_pipex *data)
 	if (pid == -1)
 		msg_error("Fork failed");
 	if (pid == 0)
-	{
-		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		if (data->i == 2)
-		{
-			input_fd = open(data->av[1], O_RDONLY);
-			if (input_fd != -1)
-				dup2(input_fd, STDIN_FILENO);
-		}
-		exec(data);
-	}
+		child_handler(data, fd);
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
@@ -47,28 +53,14 @@ static void	handle_input(t_pipex *data, int ac, char **av)
 	{
 		data->i = 3;
 		data->outfile = open_file(av[ac - 1], 2);
-		if (data->outfile == -1)
-			exit(1);
 		here_doc(av[2], ac);
 	}
 	else
 	{
 		data->i = 2;
 		data->outfile = open_file(av[ac - 1], 1);
-		if (data->outfile == -1)
-			exit(1);
 		data->infile = open_file(av[1], 0);
 	}
-}
-
-static void	init_bonus(t_pipex *data, int ac, char **av, char **envp)
-{
-	data->ac = ac;
-	data->av = av;
-	data->envp = envp;
-	data->cmd_args = NULL;
-	data->cmd_path = NULL;
-	data->cmd_paths = NULL;
 }
 
 static int	execute_last_command(t_pipex *data)
@@ -79,11 +71,18 @@ static int	execute_last_command(t_pipex *data)
 	last_pid = fork();
 	if (last_pid == 0)
 	{
-		setup_output_redirect(data->outfile);
+		if (data->outfile == -1)
+		{
+			parent_free(data);
+			exit(1);
+		}
+		dup2(data->outfile, STDOUT_FILENO);
+		close(data->outfile);
 		exec(data);
 	}
 	if (data->outfile != -1)
 		close(data->outfile);
+	close(STDIN_FILENO);
 	waitpid(last_pid, &status, 0);
 	parent_free(data);
 	if (WIFEXITED(status))
@@ -94,10 +93,16 @@ static int	execute_last_command(t_pipex *data)
 int	main(int ac, char **av, char **envp)
 {
 	t_pipex	data;
+	int		exit_code;
 
 	if (ac < 5)
 		return (msg_error("Invalid number of arguments"), 1);
-	init_bonus(&data, ac, av, envp);
+	data.ac = ac;
+	data.av = av;
+	data.envp = envp;
+	data.cmd_args = NULL;
+	data.cmd_path = NULL;
+	data.cmd_paths = NULL;
 	handle_input(&data, ac, av);
 	parse_cmds(&data);
 	while (data.i < ac - 2)
@@ -105,5 +110,8 @@ int	main(int ac, char **av, char **envp)
 		child_process(&data);
 		data.i++;
 	}
-	return (execute_last_command(&data));
+	exit_code = execute_last_command(&data);
+	while (wait(NULL) > 0)
+		;
+	return (exit_code);
 }
